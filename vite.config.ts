5681c8976b71f44ada5605e8d99598e016b9a460
Liem Nguyen <liemnt.uit@gmail.com>
2025-11-04 15:26:48 +0700

    feat(airmoney-degn/airmoney-cli-js#20): Remove unused assets and metadata files; update controller SDK version in package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { version } from './package.json';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
-import tailwindcss from '@tailwindcss/vite';
-
-const outDir = './dist';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { version } from "./package.json";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: './assets',
-          dest: './',
-        },
-        {
-          src: './metadata.json',
-          dest: './',
-        },
-        {
-          src: './dapp-logo.png',
-          dest: './',
-        },
-      ],
-    }),
-    tailwindcss(),
-  ],
-  build: {
-    outDir,
-    copyPublicDir: false,
-  },
+  plugins: [react(), tailwindcss()],
   define: {
-    'import.meta.env.AIRMONEY_PACKAGE_VERSION': JSON.stringify(version),
+    "import.meta.env.AIRMONEY_PACKAGE_VERSION": JSON.stringify(version),
   },
-  publicDir: 'assets',
 });
